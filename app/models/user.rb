041f105a8465957f9df25fa6f7c0d95e35409864
Tony020906 <angkaithong2013@gmail.com>
2020-12-27 13:33:21 +0800

    Add signup form and route

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    before_save { self.email = email.downcase }
-    has_many :articles
-    validates :username, uniqueness: {case_sensitive: false}, presence: true, length: { minimum: 3, maximum: 25}
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, uniqueness: {case_sensitive: false}, presence: true, length: { maximum: 105}, format: {with: VALID_EMAIL_REGEX}
-    has_secure_password
+  before_save { self.email = email.downcase }
+  has_many :articles
+  validates :username, uniqueness: { case_sensitive: false }, presence: true, length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, uniqueness: { case_sensitive: false }, presence: true, length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
